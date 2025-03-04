10ab2f8372e55d3e3c64b17a4f209eb73b9cc554
AkiraGhost2077 <akiramomentum@gmail.com>
2025-03-04 10:53:00 +0100

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,9 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df=pd.read_csv("train.csv")
+csv_url = "https://raw.githubusercontent.com/AkiraGhost2077/Prevision_Succes_Film/main/train.csv"
 
+@st.cache_data
+def load_data():
+    return pd.read_csv(csv_url)
+
+df = load_data()
+
+# 🔥 Interface Streamlit
 st.title("Projet de classification binaire Titanic")
 st.sidebar.title("Sommaire")
-pages=["Exploration", "DataVizualization", "Modélisation"]
-page=st.sidebar.radio("Aller vers", pages)
+pages = ["Exploration", "DataVizualization", "Modélisation"]
+page = st.sidebar.radio("Aller vers", pages)
+
+st.write("✅ Fichier CSV chargé avec succès !")
+st.dataframe(df.head())  # Afficher les premières lignes du DataFrame
